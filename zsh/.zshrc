9a7cb3bee9435cc5234f698dc8633ea32dada83e
Roshan Giyanani <roshangiyanani@gmail.com>
2019-11-17 17:37:48 -0500

    update zsh prompt install instructions
    - no longer modify ZDOTDIR (caused local zsh files to appear in repo)
    - now source the set_prompt script directly

--- zsh/.zshrc
+++
@@ -1,5 +0,0 @@
-# Set ZDOTDIR to this folder
-# Can be done writing:
-# export ZDOTDIR="[THIS_DIR]"
-# to the ~/.zshenv file.
-PROMPT="%F{green}%n%f@%F{yellow}%m%f:%F{cyan}%1/%f%# "
---
+++ zsh/set_prompt.zsh
@@ -0,0 +1,5 @@
+# source this file from your .zshrc
+# Can be done writing:
+# source [THIS_DIR]/set_prompt.zsh
+# to the ~/.zshrc file.
+PROMPT="%F{green}%n%f@%F{yellow}%m%f:%F{cyan}%1/%f%# "
