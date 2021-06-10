7fdb9904ee7abb7af0050666027ff9a507c8f9f2
Roshan Giyanani <roshan@orpheus.local>
2021-06-10 00:20:08 -0400

    improve zsh prompt with last command status emoji and right prompt time

--- zsh/set_prompt.zsh
+++ zsh/set_prompt.zsh
@@ -2,4 +2,6 @@
 # Can be done writing:
 # source [THIS_DIR]/set_prompt.zsh
 # to the ~/.zshrc file.
-PROMPT="%F{green}%n%f@%F{yellow}%m%f:%F{cyan}%1/%f%# "
+# last command failed
+PROMPT="%(?.✅.❌) %F{green}%n%f@%F{yellow}%m%f:%F{cyan}%1/%f%# "
+RPROMPT="%*"
